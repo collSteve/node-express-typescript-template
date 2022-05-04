6e85006eaed61d13b1083ffb91e9b58862a1e556
Patrick Noda <patricknoda11@gmail.com>
2022-05-03 21:14:07 -0700

    implemented user and user-service

--- src/models/game_users_info_model.ts
+++ src/models/game_users_info_model.ts
@@ -1,3 +1,6 @@
+import { userInfo } from "os";
+import TempUserModel from "./temp-user";
+
 export enum GameType {
   One_on_one,
   Two_groups_combat,
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,17 @@
 // A class representing a User
-export class UserModel {
-  private static readonly MAX_GAMES = 5;
-  private userId: string;
-  private userName: string;
+export default class UserModel {
+  private readonly MAX_GAMES = 5;
+  protected userId: string;
   private password: string;
   private games: Set<string> = new Set<string>(); // set of game ids
 
   /**
    * @constructor
    * @param {string} userId - the user id
-   * @param {string} userName - the username associated with user
    * @param {string} password - password associated with user account
    */
-  constructor(userId: string, userName: string, password: string) {
+  constructor(userId: string, password: string) {
     this.userId = userId;
-    this.userName = userName;
     this.password = password;
   }
 
@@ -24,11 +21,13 @@ export class UserModel {
    * @param {string} gameId - game id to be linked with user
    */
   public joinGame(gameId: string) {
-    this.games.add(gameId);
+    if (this.games.size < this.MAX_GAMES) {
+      this.games.add(gameId);
+    }
   }
 
   /**
-   * Removes a game from the games this user is associated with
+   * Removes a game that this user is associated with
    * @param {string} gameId - the gameId of the game to be removed
    */
   public removeGame(gameId: string) {
@@ -49,17 +48,8 @@ export class UserModel {
     return this.userId;
   }
 
-  public getUserName() {
-    return this.userName;
-  }
-
   public getPassword() {
     return this.password;
-  }
-
-  // setters:
-  public updateUserName(newUserName: string) {
-    this.userName = newUserName;
   }
 
   public updatePassword(newPassword: string) {
